6bcfe84f4ec12f921cb3ebba1730cc1b6cbb4a44
Thabang <tp.softwaredev@gmail.com>
2026-04-28 14:30:19 -0500

    Added app.yaml, .gcloudignore, and Cloud SQL socket support for GCP deployment

--- src/db.js
+++ src/db.js
@@ -1,15 +1,22 @@
 const mysql = require('mysql2/promise');
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = require('./config');
 
-const pool = mysql.createPool({
-  host: DB_HOST,
-  port: Number(DB_PORT),
+const connectionConfig = {
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-});
+};
+
+if (process.env.INSTANCE_CONNECTION_NAME) {
+  connectionConfig.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+} else {
+  connectionConfig.host = DB_HOST;
+  connectionConfig.port = Number(DB_PORT);
+}
+
+const pool = mysql.createPool(connectionConfig);
 
 module.exports = pool;
